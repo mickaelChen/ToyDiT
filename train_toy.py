683c469881ff0e2b2072bf237fa4015655f7fdeb
mickaelChen <mickael.chen@valeo.com>
2024-08-21 10:50:37 +0000

    default opt

--- train_toy.py
+++ train_toy.py
@@ -207,6 +207,6 @@ if __name__ == "__main__":
     parser.add_argument("--global-seed", type=int, default=0)
     parser.add_argument("--num-workers", type=int, default=4)
     parser.add_argument("--log-every", type=int, default=100)
-    parser.add_argument("--ckpt-every", type=int, default=50_000)
+    parser.add_argument("--ckpt-every", type=int, default=1)
     args = parser.parse_args()
     main(args)
